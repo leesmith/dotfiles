11fc8d8cfc06ae10330def61481ec9f9ffe9f2b7
Lee Smith <jeremyleesmith@gmail.com>
2025-08-22 07:48:52 -0500

    Nvim to Vim (#1)

---
+++ scripts/nvim-minpac.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Clones minpac nvim plugin manager and attempts to install plugins
+
+set -e
+
+if [[ -d "${HOME}/.config/nvim/pack/minpac" ]]; then
+  exit 0
+else
+  [ -d "${HOME}/.config/nvim/pack/minpac/opt" ] || mkdir -p "${HOME}/.config/nvim/pack/minpac/opt"
+  cd "${HOME}/.config/nvim/pack/minpac/opt"
+  git clone "git@github.com:k-takata/minpac.git" &>/dev/null
+  nvim -c 'call minpac#clean()' -c 'call minpac#update("", {"do": "quit"})'
+  exit 0
+fi
--- scripts/vim-minpac.sh
+++ scripts/vim-minpac.sh
@@ -4,12 +4,12 @@
 
 set -e
 
-if [[ -d "${HOME}/.config/nvim/pack/minpac" ]]; then
+if [[ -d "${HOME}/.vim/pack/minpac" ]]; then
   exit 0
 else
-  [ -d "${HOME}/.config/nvim/pack/minpac/opt" ] || mkdir -p "${HOME}/.config/nvim/pack/minpac/opt"
-  cd "${HOME}/.config/nvim/pack/minpac/opt"
-  git clone "git://github.com/k-takata/minpac.git" &>/dev/null
-  nvim -c 'call minpac#clean()' -c 'call minpac#update("", {"do": "quit"})'
+  [ -d "${HOME}/.vim/pack/minpac/opt" ] || mkdir -p "${HOME}/.vim/pack/minpac/opt"
+  cd "${HOME}/.vim/pack/minpac/opt"
+  git clone "git@github.com:k-takata/minpac.git" &>/dev/null
+  vim -c 'call minpac#clean()' -c 'call minpac#update("", {"do": "quit"})'
   exit 0
 fi
