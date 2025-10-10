d65752a7b982b9184eed0ce4550bced9dd6976f5
Lee Smith <jeremyleesmith@gmail.com>
2025-10-09 19:06:11 -0500

    Use .local/bin

--- install.sh
+++ install.sh
@@ -33,9 +33,9 @@ for dotfile in ${DIR}/bashrc.d/*; do
 done
 
 # Link files in bin
-[ -d "${HOME}/.config/bin" ] || mkdir "${HOME}/.config/bin"
-for program in ${DIR}/config/bin/*; do
-  ln -fs $program "${HOME}/.config/bin/$(basename $program)"
+[ -d "${HOME}/.local/bin" ] || mkdir "${HOME}/.local/bin"
+for program in ${DIR}/bin/*; do
+  ln -fs $program "${HOME}/.local/bin/$(basename $program)"
 done
 
 # Link archey4 config
